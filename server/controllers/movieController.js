d688f38e9f0d50aca8f9ab5de129ac5876de10d9
Bhanu <bhanu20cherukuri@gmail.com>
2026-05-24 14:39:32 +0530

    Added Movie fetch and search API

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -12,6 +12,57 @@ const createMovie = async (req, res) => {
   }
 };
 
+const getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const getMovieById = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+
+    if (!movie) {
+      return res.status(404).json({
+        message: "Movie not found",
+      });
+    }
+
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const searchMovies = async (req, res) => {
+  try {
+    const query = req.query.q;
+
+    const movies = await Movie.find({
+      $or: [
+        { title: { $regex: query, $options: "i" } },
+        { genres: { $regex: query, $options: "i" } },
+        { language: { $regex: query, $options: "i" } },
+        { director: { $regex: query, $options: "i" } },
+      ],
+    });
+
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 module.exports = {
   createMovie,
+  getMovies,
+  getMovieById,
+  searchMovies,
 };
--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createMovie } = require("../controllers/movieController");
+const {
+  createMovie,
+  getMovies,
+  getMovieById,
+  searchMovies,
+} = require("../controllers/movieController");
 
 router.post("/", createMovie);
+router.get("/", getMovies);
+router.get("/search", searchMovies);
+router.get("/:id", getMovieById);
 
 module.exports = router;
